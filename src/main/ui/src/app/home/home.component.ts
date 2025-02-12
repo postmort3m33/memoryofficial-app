ef8931311fc672196eecac26c79669c3da12eb9d
Joshua Touchstone <jtouch4@wgu.edu>
2025-02-12 10:27:51 -0600

    Fixed widget not loading sometimes: added stylesheet to the angular.json file, added function on nginit to load the pretixwidget, and another reload function as well. Centered text on the "Shows" category container to add in information to tell people to "Click for more info".

--- src/main/ui/src/app/home/home.component.ts
+++ src/main/ui/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ViewChild, Component, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, ViewChild, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +6,40 @@ import { AfterViewInit, ViewChild, Component, ElementRef, OnInit } from '@angula
   styleUrl: './home.component.css'
 })
 
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent implements AfterViewInit, OnInit {
   @ViewChild('bgVideo') videoElement!: ElementRef<HTMLVideoElement>;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.loadPretixWidget();
+  }
+
+  loadPretixWidget() {
+    const scriptId = 'pretix-widget-script';
+    
+    // Check if script already exists
+    if (document.getElementById(scriptId)) {
+      this.reloadPretixWidget();
+      return;
+    }
+
+    // Dynamically create script
+    const script = this.renderer.createElement('script');
+    script.id = scriptId;
+    script.type = 'text/javascript';
+    script.src = 'https://loop1tickets.com/widget/v1.en.js';
+    script.async = true;
+    script.onload = () => this.reloadPretixWidget();
+
+    this.renderer.appendChild(document.body, script);
+  }
+
+  reloadPretixWidget() {
+    if ((window as any).pretixWidget) {
+      (window as any).pretixWidget.reinitialize();
+    }
+  }
 
   // Init After View
   ngAfterViewInit() {
